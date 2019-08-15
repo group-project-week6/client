631d6a72c0b3a9f35426292a76d1f4627d7887a3
mromiario <mromiario@gmail.com>
2019-08-15 19:02:00 +0700

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <tes></tes>
+    </div>
+
+</template>
+
+<script>
+
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import tes from './Login.vue'
+export default {
+  data() {
+    return {
+      message: 'Hello world',
+    };
+  },
+  components : {
+      tes
+  }
+};
+
+
+</script>
+
+<style scoped>
+
+</style>
